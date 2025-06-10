b2c23125e1345bc8ade79747d9f96b6638203413
Jack Bennett <59575631+Bennettoyo@users.noreply.github.com>
2025-06-10 16:14:36 +0100

    Added loading/not found components, added a fetching request

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl bold"></h1>
+      Sorry but we could not find that snippet
+    </div>
+  );
+}
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+interface SnippetShowPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => {
+    setTimeout(r, 2000);
+  });
+
+  const { id } = await props.params;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
+}
